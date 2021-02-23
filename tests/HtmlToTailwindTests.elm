5fa83eec5f6260bb807b01073f9eb8f3f0b1a3fa
Dillon Kearns <dillon@dillonkearns.com>
2021-02-23 15:11:08 -0800

    Generate tailwind values from class attribute.

--- tests/HtmlToTailwindTests.elm
+++ tests/HtmlToTailwindTests.elm
@@ -13,6 +13,11 @@ suite =
                 "<div></div>"
                     |> htmlToElmTailwindModules
                     |> Expect.equal "div [] []"
+        , test "div with class" <|
+            \() ->
+                """<div class="mt-2 text-3xl font-extrabold text-gray-900"></div>"""
+                    |> htmlToElmTailwindModules
+                    |> Expect.equal "div [ css [ Tw.mt_2, Tw.text_3xl, Tw.font_extrabold, Tw.text_gray_900 ] ] []"
         ]
 
 
@@ -41,8 +46,34 @@ nodeToElm node =
 
         Html.Parser.Element elementName attributes children ->
             elementName
-                ++ " [] []"
+                ++ " ["
+                ++ (List.map
+                        (attributeToElm >> surroundWithSpaces)
+                        attributes
+                        |> String.join ", "
+                   )
+                ++ "] []"
                 |> Just
 
         Html.Parser.Comment string ->
             Nothing
+
+
+attributeToElm : Html.Parser.Attribute -> String
+attributeToElm ( name, value ) =
+    if name == "class" then
+        let
+            twValues =
+                value
+                    |> String.split " "
+                    |> List.map (\className -> "Tw." ++ String.replace "-" "_" className)
+        in
+        "css [ " ++ String.join ", " twValues ++ " ]"
+
+    else
+        "TODO"
+
+
+surroundWithSpaces : String -> String
+surroundWithSpaces string =
+    " " ++ string ++ " "
