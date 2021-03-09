e5f8941253defcebc110d5f6c5bfbf6a6077df93
Dillon Kearns <dillon@dillonkearns.com>
2021-03-08 17:49:32 -0800

    Extract a test helper.

--- tests/HtmlToElmTest.elm
+++ tests/HtmlToElmTest.elm
@@ -8,42 +8,49 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "tests"
-        [ test "html-to-elm" <|
-            \_ ->
-                let
-                    errorThing =
-                        """view : Html msg
-view = Debug.todo \"\"\"<a href="#">Link</a>\"\"\""""
-
-                    expected : String
-                    expected =
-                        """module A exposing (..)
-
+        [ testCase "html-to-elm"
+            """
 import Html exposing (..)
 import Html.Attributes as Attr
-
-
-view : Html msg
+"""
+            """view : Html msg
+view = Debug.todo \"\"\"<a href="#">Link</a>\"\"\""""
+            """view : Html msg
 view =
         a
         [ Attr.href "#"
         ]
-          [ text "Link" ]
-    """
-                            |> String.replace "\u{000D}" ""
-                in
-                """module A exposing (..)
+          [ text "Link" ]"""
+        ]
 
-import Html exposing (..)
-import Html.Attributes as Attr
 
+testCase name imports errorThing expectedBody =
+    test name <|
+        \_ ->
+            let
+                expected : String
+                expected =
+                    """module A exposing (..)
+"""
+                        ++ imports
+                        ++ """
 
 """
-                    ++ errorThing
-                    |> String.replace "\u{000D}" ""
-                    |> Review.Test.run HtmlToElm.rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error { message = "Here's my attempt to complete this stub", details = [ "" ], under = errorThing }
-                            |> Review.Test.whenFixed expected
-                        ]
-        ]
+                        ++ expectedBody
+                        ++ """
+    """
+                        |> String.replace "\u{000D}" ""
+            in
+            """module A exposing (..)
+"""
+                ++ imports
+                ++ """
+
+"""
+                ++ errorThing
+                |> String.replace "\u{000D}" ""
+                |> Review.Test.run HtmlToElm.rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error { message = "Here's my attempt to complete this stub", details = [ "" ], under = errorThing }
+                        |> Review.Test.whenFixed expected
+                    ]
