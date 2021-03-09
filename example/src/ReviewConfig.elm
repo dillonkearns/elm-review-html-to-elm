f2d2ba48d79ab87d9689bf128aaa74d348b86c31
Dillon Kearns <dillon@dillonkearns.com>
2021-03-08 19:21:17 -0800

    Add example config.

---
+++ example/src/ReviewConfig.elm
@@ -0,0 +1,17 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+To add packages that contain rules, add them to this review project using
+    `elm install author/packagename`
+when inside the directory containing this file.
+-}
+
+import HtmlToElm
+import Review.Rule exposing (Rule)
+
+
+config : List Rule
+config =
+    [ HtmlToElm.rule
+    ]
