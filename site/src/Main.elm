b4b461111cf70e555883ce3bffe3483da6d62743
Dillon Kearns <dillon@dillonkearns.com>
2021-03-08 21:30:31 -0800

    Update site.

--- site/src/Main.elm
+++ site/src/Main.elm
@@ -230,6 +230,7 @@ view model =
                     |> text
                 ]
             ]
+        , footerView
         ]
         |> toUnstyled
 
@@ -717,4 +718,151 @@ copyIcon =
             , SvgAttr.d "M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
             ]
             []
+        ]
+
+
+footerView : Html msg
+footerView =
+    {- This example requires Tailwind CSS v2.0+ -}
+    footer
+        [ css
+            [ Tw.bg_white
+            , Tw.flex
+            , Tw.justify_around
+            ]
+        , attribute "aria-labelledby" "footerHeading"
+        ]
+        [ div
+            [ css
+                [ Tw.max_w_7xl
+
+                --, Tw.mx_auto
+                , Tw.py_4
+                , Tw.px_4
+
+                --, Tw.justify_around
+                --, Bp.lg
+                --    [ Tw.py_16
+                --    , Tw.px_8
+                --    ]
+                --, Bp.sm
+                --    [ Tw.px_6
+                --    ]
+                ]
+            ]
+            [ div
+                [ css
+                    []
+                ]
+                [ div
+                    [ css
+                        [ Tw.space_x_8
+                        , Tw.flex
+                        , Tw.items_center
+                        ]
+                    ]
+                    [ img
+                        [ css
+                            [ Tw.h_6
+                            ]
+                        , Attr.src "https://res.cloudinary.com/dillonkearns/image/upload/v1614626535/Incremental_Elm_Logo_aeb8qs.png"
+                        , Attr.alt "Incremental Elm"
+                        ]
+                        []
+                    , p
+                        [ css
+                            [ Tw.text_gray_500
+                            , Tw.text_base
+                            ]
+                        ]
+                        [ text "Brought to you by Dillon Kearns. Check out more of my work at incrementalelm.com." ]
+                    , div
+                        [ css
+                            [ Tw.flex
+                            , Tw.space_x_6
+                            ]
+                        ]
+                        [ a
+                            [ Attr.href "https://twitter.com/dillontkearns"
+                            , Attr.rel "noopener"
+                            , Attr.target "_blank"
+                            , css
+                                [ Tw.text_gray_400
+                                , Css.hover
+                                    [ Tw.text_gray_500
+                                    ]
+                                ]
+                            ]
+                            [ span
+                                [ css
+                                    [ Tw.sr_only
+                                    ]
+                                ]
+                                [ text "Twitter" ]
+                            , Svg.svg
+                                [ SvgAttr.css
+                                    [ Tw.h_6
+                                    , Tw.w_6
+                                    ]
+                                , SvgAttr.fill "currentColor"
+                                , SvgAttr.viewBox "0 0 24 24"
+                                , attribute "aria-hidden" "true"
+                                ]
+                                [ Svg.path
+                                    [ SvgAttr.d "M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+                                    ]
+                                    []
+                                ]
+                            ]
+                        , a
+                            [ Attr.href "https://github.com/dillonkearns/"
+                            , Attr.rel "noopener"
+                            , Attr.target "_blank"
+                            , css
+                                [ Tw.text_gray_400
+                                , Css.hover
+                                    [ Tw.text_gray_500
+                                    ]
+                                ]
+                            ]
+                            [ span
+                                [ css
+                                    [ Tw.sr_only
+                                    ]
+                                ]
+                                [ text "GitHub" ]
+                            , Svg.svg
+                                [ SvgAttr.css
+                                    [ Tw.h_6
+                                    , Tw.w_6
+                                    ]
+                                , SvgAttr.fill "currentColor"
+                                , SvgAttr.viewBox "0 0 24 24"
+                                , attribute "aria-hidden" "true"
+                                ]
+                                [ Svg.path
+                                    [ SvgAttr.fillRule "evenodd"
+                                    , SvgAttr.d "M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                    , SvgAttr.clipRule "evenodd"
+                                    ]
+                                    []
+                                ]
+                            ]
+                        ]
+                    ]
+                , div
+                    [ css
+                        [--Tw.mt_12
+                         --, Tw.grid
+                         --, Tw.grid_cols_2
+                         --, Tw.gap_8
+                         --, Bp.xl
+                         --    [ Tw.mt_0
+                         --    , Tw.col_span_2
+                         --    ]
+                        ]
+                    ]
+                    []
+                ]
+            ]
         ]
